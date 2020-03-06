c4643a664c5d614163acd25c09ccc921478c5118
Andrew McMahon <andrewmcm26@gmail.com>
2020-03-06 12:12:19 -0600

    Added audio buttons

--- Musicalytics/Models/AudioEmotion.cs
+++ Musicalytics/Models/AudioEmotion.cs
@@ -11,5 +11,6 @@ namespace Musicalytics.Models
         public string emotionName { get; set; }
         public string songName { get; set; }
         public string artistName { get; set; }
+        public string audio = "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview118/v4/79/4e/f4/794ef45b-f9e1-27c3-c143-d29061d6863f/mzaf_5008234458169035990.plus.aac.p.m4a";
     }
 }
