9b33225df36c5dcf2723e5a691c265a26c139d6f
William Dodson <wcdodson4@gmail.com>
2020-03-11 13:00:34 -0500

    added About page framework

--- Musicalytics/Controllers/HomeController.cs
+++ Musicalytics/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace Musicalytics.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
